09ca2e116cc8d129760655e7610e98c6be4d4bdc
NimeshRukshan <fernandownr.21@uom.lk>
2024-11-23 17:53:44 +0530

    Update Azure deployment workflow to use new secret key names and refine CORS origin configuration

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
   })
 );
 app.use(express.json());
